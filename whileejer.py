9be6e6f071cc53de2fa4d72b58d87dc3b352a748
Francis2412 <francisalejandrasm@gmail.com>
2026-08-30 21:13:15 -0600

    Ejercicios de while terminados

--- whileejer.py
+++ whileejer.py
@@ -1,7 +1,15 @@
 def Cierrecaja():
     #Ingresa los montos de ventas hasta escribir 0.
     # Calcula el total recaudado y la cantidad de ventas.
-    print("")
+    total= 0
+    ventas = 0
+    venta = int(input("Ingrese la cantidad a vender: "))
+    while venta != 0:
+        total += venta
+        ventas += 1
+        venta = int(input("Ingrese la cantidad a vender: "))
+    print(f"El total recaudado es igual a: {total}")
+    print(f"La cantidad de ventas fue: {ventas}")
 
 def Accesosistema():
     #Solicita la clave hasta que sea correcta. 
@@ -19,20 +27,43 @@ def Accesosistema():
     print(f"Se necesitaron {intentos + 1} intentos.")
 
 
-    print("")
+
 
 def Cantidadpedido():
     #Un distribuidor acepta de 1 a 100 unidades. 
     #Solicita la cantidad hasta que sea válida y luego calcula el total.
-    print("")
+    cantidad = int(input("Ingresa la cantiad de unidades que deseas: "))
+    while cantidad < 1 or cantidad > 100:
+        print("Cantidad inválida. Debe estar entre 1 y 100")
+        cantidad = int(input("Ingresa la cantiad de unidades que deseas: "))
+
+    print(f"El total es igual a {cantidad * 30} córdobas")
+    
 
 def Combustiblereparto():
     #Una motocicleta inicia con 8 litros. 
     #Registra el consumo de cada recorrido
     #mientras quede combustible y alerta al llegar a 1 litro.
-    print("")
+    consumo = 8
+    gasolina = int(input("Ingrese cuanta gasolina consumio: "))
+    while consumo  > 1:
+        consumo -=  gasolina
+        if consumo  > 1:
+          gasolina = int(input("Ingrese cuanta gasolina consumio: "))
+
+    print ("¡Alerta! se esta quedando sin gasolina, solo queda 1 litro")
+
+
+    
 
 def Reposiciónexistencias():
     #Una tienda tiene 3 unidades y desea llegar a 20.
     #Solicita cada reposición y termina al alcanzar o superar la meta.
-    print("")
+    unidades = 3
+    
+    while unidades < 20:
+        reposicion  = int(input("Ingrese la cantidad a reponer: "))
+        unidades += reposicion
+    print(f"La tienda ahora tiene {unidades} unidades")
+        
+
