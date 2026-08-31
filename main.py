18d1a02700af2db553c8302c5b8a8861db372a49
Francis2412 <francisalejandrasm@gmail.com>
2026-08-30 21:49:38 -0600

    main lista

--- main.py
+++ main.py
@@ -13,7 +13,44 @@ def ejemplo ():
         print(f"Nota{notas.index(j)+1}: {j}")
 
 def main():
-   print("")
+    print("*********** MENU DE LOS EJERCICIOS ***********")
+    print("********** Ejercicios de bucles for **********")
+    print("1.......................Ventas de un minisúper")
+    print("2............................Recepción de café")
+    print("3.......................Revisión de inventario")
+    print("4............................Producción de pan")
+    print("5......................Evaluación del servicio")
+    print("********* Ejercicios de bucles while *********")
+    print("6...............................Cierre de caja")
+    print("7............................Acceso al sistema")
+    print("8........................Cantidad de un pedido")
+    print("9.......................Combustible de reparto")
+    print("10...................Reposición de existencias")
+    opc = int(input("Seleccione la tarea a ejecutarse: "))
+    match opc:
+        case 1:
+            ventasminisuper()
+        case 2:
+            Recepcióncafé()
+        case 3: 
+            Revisióninventario()
+        case 4:
+            Producciónpan()
+        case 5:
+            Evaluaciónservicio()
+        case 6:
+            Cierrecaja()
+        case 7:
+            Accesosistema()
+        case 8:
+            Cantidadpedido()
+        case 9:
+            Combustiblereparto()
+        case 10:
+            Reposiciónexistencias()
+
+
+main()
 
 
 main()
